f1393bf7c57f54ad42c1063e56491d1e438dde32
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-07-05 21:51:08 -0300

    Add files via upload

---
+++ alisson_legal_aval_02_07.py
@@ -0,0 +1,23 @@
+'''
+fup em python que leia o dia e o mes de seu nascimento e imprima a estacao do ano em que voce nasceu
+
+saida esperada:
+Insira o mes do ano (exemplo: janeiro, fevereiro etc.: julho
+Insira o dia do mês: 10
+Estação do ano: inverno
+'''
+
+
+mes = input("Insira o mês do ano (exemplo: janeiro, fevereiro etc.): ")
+dia = int(input("Insira o dia do mês: "))
+
+if mes == 'julho' and dia == 10:
+    print("Estação do ano: inverno")
+
+
+
+
+
+
+# else:
+#     print("não é a saida esperada")
