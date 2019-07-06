1e14b5947e9cd1da2b6d15886c1acf3ce748a388
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-07-05 22:31:01 -0300

    Add files via upload

---
+++ alisson_legal_aval_02_02.py
@@ -0,0 +1,10 @@
+'''
+fup em python que remova elementos duplicados de uma determinada lista. nao use a estrutura de dados
+list do python
+'''
+
+lista = ['a', 'l', 'i', 's', 's', 'o', 'n']
+print((lista), "\n")
+
+lista.remove('s')
+print(lista)
